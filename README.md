870aa86d47263f6ff7449b535296ddeffd98d239
patand08 <patrickandrade08@hotmail.com>
2024-08-26 18:50:00 -0300

    Handle form submition

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ Basic Next.js issue tracker:
 Next.js
 Tailwind
 Radix UI\*
+React SimpleMDE
+React Hook Form
+Axios
 Prisma + MySQL
 
 \*(note that Radix and Next above version 13 has a problem with the imports of css files, therefore, the font is the default one for Radix and not Inter. I don't want to fix that)
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,39 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE placeholder="Issue description." />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root placeholder="Title" {...register("title")} />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Issue description." {...field} />
+        )}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 
