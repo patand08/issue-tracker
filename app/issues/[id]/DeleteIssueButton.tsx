5fbc1d4fcd3f1e0a7aa7fd82b4acf4368f932930
patand08 <patrickandrade08@hotmail.com>
2024-08-28 19:05:21 -0300

    Improve delete button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,43 +1,80 @@
 "use client";
+import { Spinner } from "@/app/components";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
 
+  const [error, setError] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const onDelete = async () => {
+    try {
+      setIsDeleting(true);
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setIsDeleting(false);
+      setError(true);
+    }
+  };
+
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">Delete</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delete this issue? This action can't be
-          undone.
-        </AlertDialog.Description>
-        <Flex mt="4" gap="4" justify="end">
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              color="red"
-              onClick={async () => {
-                await axios.delete("/api/issues/" + issueId);
-                router.push("/issues");
-                router.refresh();
-              }}
-            >
-              Delete
-            </Button>
-          </AlertDialog.Action>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red" disabled={isDeleting}>
+            {isDeleting ? <Spinner /> : "Delete"}
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue? This action can't be
+            undone.
+          </AlertDialog.Description>
+          <Flex mt="4" gap="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={onDelete}>
+                Delete
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <Flex>
+          <AlertDialog.Content maxWidth="300px">
+            <AlertDialog.Title>Error</AlertDialog.Title>
+            <AlertDialog.Description>
+              This issue could not be deleted.
+            </AlertDialog.Description>
+
+            <Flex mt="4" justify="end">
+              <Button
+                color="gray"
+                variant="soft"
+                onClick={() => {
+                  setError(false);
+                }}
+              >
+                Ok
+              </Button>
+            </Flex>
+          </AlertDialog.Content>
         </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+      </AlertDialog.Root>
+    </>
   );
 };
 
