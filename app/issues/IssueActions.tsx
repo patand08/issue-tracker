95383b24b35d0f88f3a18c55702f351b8a0185c2
patand08 <patrickandrade08@hotmail.com>
2024-09-02 12:10:05 -0300

    Extra: asc/desc + clear

--- app/issues/IssueActions.tsx
+++ app/issues/IssueActions.tsx
@@ -1,12 +1,19 @@
+"use client";
 import { Button, Flex } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 import IssueStatusFilter from "./IssueStatusFilter";
+import { useSearchParams } from "next/navigation";
 
 const IssueActions = () => {
+  const searchParams = useSearchParams();
+
   return (
     <Flex mb="5" justify="between">
-      <IssueStatusFilter />
+      <Flex align="center" gap="3">
+        <IssueStatusFilter />{" "}
+        {searchParams.size > 0 && <Link href="/issues">Clear</Link>}
+      </Flex>
       <Button>
         <Link href="/issues/new">New Issue</Link>
       </Button>
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,10 +4,14 @@ import { IssueStatusBadge, Link } from "../components";
 import IssueActions from "./IssueActions";
 import { Issue, Status } from "@prisma/client";
 import NextLink from "next/link";
-import { ArrowUpIcon } from "@radix-ui/react-icons";
+import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
 
 interface Props {
-  searchParams: { status: Status; orderBy: keyof Issue };
+  searchParams: {
+    status: Status;
+    orderBy: keyof Issue;
+    sortOrder: "asc" | "desc";
+  };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
@@ -22,10 +26,16 @@ const IssuesPage = async ({ searchParams }: Props) => {
     ? searchParams.status
     : undefined;
 
+  const toggleOrder = () => {
+    return !searchParams.sortOrder || searchParams.sortOrder === "desc"
+      ? "asc"
+      : "desc";
+  };
+
   const orderBy = columns
     .map((column) => column.value)
     .includes(searchParams.orderBy)
-    ? { [searchParams.orderBy]: "asc" }
+    ? { [searchParams.orderBy]: searchParams.sortOrder }
     : undefined;
 
   const issues = await prisma.issue.findMany({
@@ -48,14 +58,24 @@ const IssuesPage = async ({ searchParams }: Props) => {
               >
                 <NextLink
                   href={{
-                    query: { ...searchParams, orderBy: column.value },
+                    query: {
+                      ...searchParams,
+                      orderBy: column.value,
+                      sortOrder:
+                        searchParams.orderBy === column.value
+                          ? toggleOrder()
+                          : "asc",
+                    },
                   }}
                 >
                   {column.label}
                 </NextLink>
-                {column.value === searchParams.orderBy && (
-                  <ArrowUpIcon className="inline" />
-                )}
+                {column.value === searchParams.orderBy &&
+                  (searchParams.sortOrder === "asc" ? (
+                    <ArrowUpIcon className="inline" />
+                  ) : (
+                    <ArrowDownIcon className="inline" />
+                  ))}
               </Table.ColumnHeaderCell>
             ))}
           </Table.Row>
