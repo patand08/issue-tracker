3ccc7f20b8f8490636d5dcbfa5a4f2f643459a74
patand08 <patrickandrade08@hotmail.com>
2024-09-02 12:25:40 -0300

    Build layout pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Pagination pageSize={10} itemCount={102} currentPage={2} />
+    </div>
+  );
 }
