fbb1e8325ae4525055283588a5bda53e3e0aa097
patand08 <patrickandrade08@hotmail.com>
2024-09-02 13:19:57 -0300

    extract IssueTable

--- app/issues/IssueActions.tsx
+++ app/issues/IssueActions.tsx
@@ -7,12 +7,14 @@ import { useSearchParams } from "next/navigation";
 
 const IssueActions = () => {
   const searchParams = useSearchParams();
+  const validation =
+    searchParams.size > 0 &&
+    !(searchParams.size === 1 && searchParams.get("page"));
 
   return (
-    <Flex mb="5" justify="between">
+    <Flex justify="between">
       <Flex align="center" gap="3">
-        <IssueStatusFilter />{" "}
-        {searchParams.size > 0 && <Link href="/issues">Clear</Link>}
+        <IssueStatusFilter /> {validation && <Link href="/issues">Clear</Link>}
       </Flex>
       <Button>
         <Link href="/issues/new">New Issue</Link>
